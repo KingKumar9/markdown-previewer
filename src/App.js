145bdc329fde37c888dec0c7937a71ffde913894
Rajat Kumar <rajatk12199@gmail.com>
2021-12-02 06:13:07 +1100

    final changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React from 'react';
+import { marked } from 'marked';
 import './App.css';
+import rawMarkdown from './markdown'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      input: rawMarkdown
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(e) {
+    this.setState({
+      input: e.target.value
+    })
+  }
+
+  getRawMarkup() {
+    return { __html: marked(this.state.input) }
+  }
+
+
+  render() {
+    marked.setOptions({
+      breaks: true
+    })
+
+    return (
+      <div className="container">
+        <div className="titleContainer">
+          <h1 className="title">markdown previewer</h1>
+        </div>
+        <div className="bodyContainer">
+          <div className="inputContainer">
+            <h3 className="inputTitle">input</h3>
+            <textarea id="editor" className="editor" onChange={this.handleChange} value={this.state.input}></textarea>
+          </div>
+          <div className="outputContainer">
+            <h3 className="outputTitle">output</h3>
+            <div id="preview" className="preview" dangerouslySetInnerHTML={this.getRawMarkup()}/>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
